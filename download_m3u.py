ee905ba3de33bfcb06c2a0501514e36d0f5861f9
karepech <karepechannel.real@gmail.com>
2026-05-17 09:42:08 +0700

    Update download_m3u.py

--- download_m3u.py
+++ download_m3u.py
@@ -26,7 +26,7 @@ MASTER_URLS = [
     "https://freeiptv2026.tsender57.workers.dev",
     "https://cixar.web.id/gvision1.m3u",
     "https://raw.githubusercontent.com/pk0979/m3u-live/refs/heads/main/all.m3u",
-    "https://liveevent.iptvbonekoe.workers.dev",
+    "https://raw.githubusercontent.com/Love4vn/Stalker2M3U/refs/heads/1/Football_match_live.m3u",
     "https://raw.githubusercontent.com/tsender57-dotcom/iptv-rox-playlist/refs/heads/main/rakettv.m3u8",
     "https://bit.ly/KPL203"
 ]
