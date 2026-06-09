16a7b4807690b74ce19a5a2f09d43bc261b55d8c
karepech <karepechannel.real@gmail.com>
2026-06-09 23:02:44 +0700

    Add new master URL for m3u playlist

--- download_m3u.py
+++ download_m3u.py
@@ -15,6 +15,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # ====================================================================
 
 MASTER_URLS = [
+    "https://fireredst.github.io/Itz-Play/Play.m3u",
     "https://raw.githubusercontent.com/DinasKegelapanKotaKonoha/romlah-gunadi/refs/heads/main/romlahsiahaan.m3u", 
     "https://gist.githubusercontent.com/zentral-qtrung/531720c17d089702dd7ac4b96db6e1ce/raw/a40d137328263e58d827dccee2974079eae092b5/sports.m3u",
     "https://bwifi.my.id/maling.m3u",
